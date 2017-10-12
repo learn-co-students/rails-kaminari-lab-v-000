653769546edf950846ea7d2467aa4b4c994f24a8
debjohnson33 <debjohnson_11@yahoo.com>
2017-10-12 13:51:48 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.order(created_at: :desc).page(params[:page])
+    @songs = Song.order(title: :asc).page(params[:page])
   end
 
   def show
